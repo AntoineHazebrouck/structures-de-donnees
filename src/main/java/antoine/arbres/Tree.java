fe7e329a10426d74b73d90df9e8b3c606d82f0ed
Antoine Hazebrouck <antoine.haz@gmail.com>
2024-10-19 10:50:19 +0200

    tree add

---
+++ src/main/java/antoine/arbres/Tree.java
@@ -0,0 +1,42 @@
+package antoine.arbres;
+
+import lombok.Data;
+
+@Data
+public class Tree {
+	private final int value;
+	private Tree left;
+	private Tree right;
+
+	public Tree(int value) {
+		this.value = value;
+		left = null;
+		right = null;
+	}
+
+	public Tree(Tree left, int value, Tree right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
+	}
+
+	public void add(Integer value) {
+		if (value > this.value) {
+			if (isLeaf()) {
+				right = new Tree(value);
+			} else {
+				right.add(value);
+			}
+		} else if (value < this.value) {
+			if (isLeaf()) {
+				left = new Tree(value);
+			} else {
+				left.add(value);
+			}
+		}
+	}
+
+	public boolean isLeaf() {
+		return left == null && right == null;
+	}
+}
---
+++ src/test/java/antoine/arbres/TreeTest.java
@@ -0,0 +1,21 @@
+package antoine.arbres;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TreeTest {
+	@Test
+	void testAdd() {
+		Tree tree = new Tree(new Tree(5), 10, new Tree(15));
+
+		tree.add(7);
+
+		assertThat(tree).isEqualTo(
+			new Tree(
+				new Tree(null, 5, new Tree(7)), 
+				10, 
+				new Tree(15))
+		);
+	}
+}
